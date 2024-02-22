0e1959151202d7f55ee5291a2d096e7ced5046a9
Sebastian Søndergaard Knakkergaard <ses@mjolner.dk>
2024-02-22 10:03:49 +0100

    Add usage example with larger objects

--- SimpleObjectDb/Program.cs
+++ SimpleObjectDb/Program.cs
@@ -9,6 +9,7 @@ var stopwatch = new Stopwatch();
 
 var config = new SimpleObjectDbConfiguration();
 config.RegisterDataType<TestObjectA>(i => i.Id);
+config.RegisterDataType<TestObjectB>(i => i.Id);
 
 //ISimpleObjectDb db = new SimpleFileObjectDb(@"C:\Temp\SimpleObjectDb\", config);
 
@@ -16,13 +17,16 @@ string sqlServerConnectionString = @"Server=localhost,9001;Database=SimpleObject
 SimpleSqlServerObjectDb.CreateIfNotExist(sqlServerConnectionString, config);
 ISimpleObjectDb db = new SimpleSqlServerObjectDb(sqlServerConnectionString, config);
 
+// ==============================
+// Test with many small objects
+// ==============================
 
-var testObjects = fixture.CreateMany<TestObjectA>(10000);
+var testObjectAList = fixture.CreateMany<TestObjectA>(10000);
 
 
-Console.WriteLine("Creating items in db");
+Console.WriteLine("Creating many items in db");
 stopwatch.Start();
-foreach (var item in testObjects)
+foreach (var item in testObjectAList)
 {
     await db.CreateAsync(item);
 }
@@ -30,9 +34,9 @@ stopwatch.Stop();
 Console.WriteLine($"Items created in db in {stopwatch.Elapsed}");
 
 
-Console.WriteLine("Fetching items from db one by one");
+Console.WriteLine("Fetching many items from db one by one");
 stopwatch.Restart();
-foreach (var item in testObjects)
+foreach (var item in testObjectAList)
 {
     var storedItem = await db.GetByIdAsync<TestObjectA>(item.Id);
 }
@@ -40,9 +44,9 @@ stopwatch.Stop();
 Console.WriteLine($"Items fetched from db in {stopwatch.Elapsed}");
 
 
-Console.WriteLine("Updating items in db one by one");
+Console.WriteLine("Updating many items in db one by one");
 stopwatch.Restart();
-foreach (var item in testObjects)
+foreach (var item in testObjectAList)
 {
     var newItem = item with { Text = fixture.Create<string>() };
     await db.UpdateAsync(newItem);
@@ -51,7 +55,7 @@ stopwatch.Stop();
 Console.WriteLine($"Items updated in db in {stopwatch.Elapsed}");
 
 
-Console.WriteLine("Fetching all items from db");
+Console.WriteLine("Fetching all (many) items from db");
 stopwatch.Restart();
 List<TestObjectA> allItems = new();
 await foreach (var item in db.GetAllAsync<TestObjectA>())
@@ -62,11 +66,50 @@ stopwatch.Stop();
 Console.WriteLine($"All items fetched from db in {stopwatch.Elapsed}");
 
 
-Console.WriteLine("Deleting items from db one by one");
+Console.WriteLine("Deleting many items from db one by one");
 stopwatch.Restart();
-foreach (var item in testObjects)
+foreach (var item in testObjectAList)
 {
     await db.DeleteByIdAsync<TestObjectA>(item.Id);
+}
+stopwatch.Stop();
+Console.WriteLine($"Items deleted from db in {stopwatch.Elapsed}");
+
+
+// ==============================
+// Test with few large objects
+// ==============================
+
+var testObjectBList = new List<TestObjectB>();
+for (int i = 0; i < 1000; i++)
+{
+    testObjectBList.Add(new TestObjectB(Guid.NewGuid(), fixture.CreateMany<TestSubObjectB>(500).ToArray()));
+}
+
+Console.WriteLine("Creating large items in db");
+stopwatch.Start();
+foreach (var item in testObjectBList)
+{
+    await db.CreateAsync(item);
+}
+stopwatch.Stop();
+Console.WriteLine($"Items created in db in {stopwatch.Elapsed}");
+
+Console.WriteLine("Updating large items in db one by one");
+stopwatch.Restart();
+foreach (var item in testObjectBList)
+{
+    var newItem = item with { Values = fixture.CreateMany<TestSubObjectB>(500).ToArray() };
+    await db.UpdateAsync(newItem);
+}
+stopwatch.Stop();
+Console.WriteLine($"Items updated in db in {stopwatch.Elapsed}");
+
+Console.WriteLine("Deleting large items from db one by one");
+stopwatch.Restart();
+foreach (var item in testObjectBList)
+{
+    await db.DeleteByIdAsync<TestObjectB>(item.Id);
 }
 stopwatch.Stop();
 Console.WriteLine($"Items deleted from db in {stopwatch.Elapsed}");
@@ -76,4 +119,8 @@ Console.WriteLine($"Items deleted from db in {stopwatch.Elapsed}");
 internal record TestObjectA(int Id, string Text, List<TestSubObjectA> SubObjects);
 
 internal record TestSubObjectA(int Id, string Text);
+
+internal record TestObjectB(Guid Id, TestSubObjectB[] Values);
+
+internal record TestSubObjectB(string A, string B, string C, string D, string E);
 
